cd25d7d573322a2c28a91883933d0a070b5f49e6
Deepak Kumar <deepak.kr10460@gmail.com>
2023-04-11 12:28:28 +0530

    initial commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,43 @@
+const Sdata = [
+  {
+    id: 1,
+    sname: "DARK",
+    imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 2,
+    sname: "extra curricular",
+    imgsrc:
+      "https://tse3.mm.bing.net/th?id=OIP.MHw-_Cm2-Drd5uIHocL4LQHaD4&pid=Api&P=0",
+    // "https://hitc.com/static/uploads/hitcn/1816/extracurricular_netflix_780_1544944.jpeg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 3,
+    sname: "Stranger Things",
+    imgsrc: "https://wallpapercave.com/wp/wp1917154.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 4,
+    sname: "The Vampire Diaries",
+    imgsrc:
+      // "https://images.justwatch.com/poster/8589251/s592.jpg"
+      "https://tse4.mm.bing.net/th?id=OIP.N2RoKro_HFPz2xZAUKJhAwHaFu&pid=Api&P=0",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 5,
+    sname: "My First-2 Love",
+    imgsrc:
+      "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+];
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Card from "./Card";
+import Sdata from "./Sdata";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <>
+    <h1 className="heading_style">Top 5 Netflix Series</h1>
+    {Sdata.map((value) => (
+      <Card
+        key={value.id}
+        imgSrc={value.imgsrc}
+        title={value.title}
+        sname={value.sname}
+        links={value.links}
+      />
+    ))}
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
